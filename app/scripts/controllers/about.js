37d98f6cad94b62d50b11cf846d19ef86bcd9bd9
unknown <chilly_bee@yahoo.com>
2015-07-14 14:06:22 +0100

    First commit cookie

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularCompetitionPricesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularCompetitionPricesApp
+ */
+angular.module('angularCompetitionPricesApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
